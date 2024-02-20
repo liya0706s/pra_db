2be14e08c7643d4cf1730269e7ad44cdb8c812db
Angie <liya0706s@gmail.com>
2024-02-20 22:16:41 +0800

    20240219的db0220打完,明天再加油

--- a20240219_db.php
+++ a20240219_db.php
@@ -18,6 +18,7 @@ class DB
         return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
 
+    // array to sql 
     private function a2s($array)
     {
         foreach ($array as $col => $value) {
@@ -39,7 +40,7 @@ class DB
                     $sql .= " where " . join(" && ", $tmp);
                 }
             } else {
-            // 有資料表，裡面的參數不是陣列
+                // 有資料表，裡面的參數不是陣列
                 $sql .= " $array";
             }
             $sql .= $other;
@@ -47,19 +48,104 @@ class DB
         }
     }
 
-    function all($where='', $other=''){
+    function all($where = '', $other = '')
+    {
         $sql = "select * from `$this->table` ";
-        $sql=$this->sql_all($sql,$where,$other);
+        $sql = $this->sql_all($sql, $where, $other);
         return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    function count($where='',$other=''){
-        $sql="select count(*) from `$this->table` ";
-        $sql=$this->sql_all($sql,$where,$other);
+    function count($where = '', $other = '')
+    {
+        $sql = "select count(*) from `$this->table` ";
+        $sql = $this->sql_all($sql, $where, $other);
         return $this->pdo->query($sql)->fetchColumn();
     }
 
-    private function math($math, $col, $array='',$other=''){
-        
+    private function math($math, $col, $array = '', $other = '')
+    {
+        $sql = "select $math(`$col`) from `$this->table` ";
+        $sql = $this->sql_all($sql, $array, $other);
+        return $this->pdo->query($sql)->fetchColumn();
     }
+
+    // EX: SELECT SUM(total_price) FROM orders WHERE YEAR(order_date) = 2023 ;
+    function sum($col = '', $where = '', $other = '')
+    {
+        return $this->math('sum', $col, $where, $other);
+    }
+
+    function max($col, $where = '', $other = '')
+    {
+        return $this->math('max', $col, $where, $other);
+    }
+
+    function min($col, $where = '', $other = '')
+    {
+        return $this->math('min', $col, $where, $other);
+    }
+
+    function find($id)
+    {
+        $sql = "select * from `$this->table` ";
+        if (is_array($id)) {
+            $tmp = $this->a2s($id);
+            $sql .= " where " . join(" && ", $tmp);
+        } else if (is_numeric($id)) {
+            $sql .= " where `id`='$id'";
+        }
+        $row = $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+        return $row;
+    }
+
+    function del($id)
+    {
+        $sql = "delete from `$this->table` where ";
+        if (is_array($id)) {
+            $tmp = $this->a2s($id);
+            $sql .= join(" && ", $tmp);
+        } else if (is_numeric($id)) {
+            $sql .= " `id`='$id'";
+        }
+        return $this->pdo->exec($sql);
+    }
+
+    function save($array)
+    {
+        if (isset($array['id'])) {
+            $sql = "update `$this->table` set ";
+            if (!empty($array)) {
+                $tmp = $this->a2s($array);
+            }
+            $sql .= join(",", $tmp);
+            $sql .= " where `id`='{$array['id']}'";
+        } else {
+            $sql = "insert into `$this->table` ";
+            $cols = "(`" . join("`,`", array_keys($array)) . "`)";
+            $vals = "('" . join("','", $array) . "')";
+            $sql = $sql . $cols . " values " . $vals;
+        }
+        return $this->pdo->exec($sql);
+    }
+}
+// class DB 到此
+
+function dd($array)
+{
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+
+$Total=new DB('total');
+
+if(!isset($_SESSION['visited'])){
+    if($Total->count(['date'=>date("Y-m-d")])>0){
+        $total=$Total->find(['date'=>date("Y-m-d")]);
+        $total['total']++;
+        $Total->save($total);
+    }else{
+        $Total->save(['total'=>1,'date'=>date("Y-m-d")]);
+    }
+    $_SESSION['visited']=1;
 }
