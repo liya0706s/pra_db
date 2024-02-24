39b468141a606d95a7b1f6e1297e9a8b6611d4d1
Angie <liya0706s@gmail.com>
2024-02-25 06:42:47 +0800

    fix: db除錯.....

---
+++ a20240224_db.php
@@ -0,0 +1,100 @@
+<?php
+date_default_timezone_set("Asia/Taipei");
+session_start();
+
+class DB
+{
+    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=web09";
+    protected $pdo;
+    protected $table;
+
+    public function __construct($table)
+    {
+        $this->table = $table;
+        $this->pdo = new PDO($this->dsn, 'root', '');
+    }
+
+    function q($sql)
+    {
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    private function a2s($array)
+    {
+        foreach ($array as $col => $value) {
+            $tmp[] = "`$col`='$value'";
+        }
+        return $tmp;
+    }
+
+    private function sql_all($sql, $array, $other)
+    {
+        if (isset($this->table) && !empty($this->table)) {
+            if (is_array($array)) {
+                if (!empty($array)) {
+                    $tmp = $this->a2s($array);
+                    $sql .= " where " . join(" && ", $tmp);
+                }
+            } else {
+                $sql .= " $array";
+            }
+            $sql .= $other;
+            return $sql;
+        }
+    }
+
+    private function math($math, $col, $array = '', $other = '')
+    {
+        $sql = "select $math(`$col`) from `$this->table` ";
+        $sql = $this->sql_all($sql, $array, $other);
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+
+    function sum($col = '', $where = '', $other = '')
+    {
+    }
+    function max($col, $where = '', $other = '')
+    {
+    }
+    function min($col, $where = '', $other = '')
+    {
+    }
+    
+
+    function find($id)
+    {
+        $sql = "select * from `$this->table` ";
+        if (is_array($id)) {
+            $tmp = $this->a2s($id);
+            $sql .= " where " . join(" && ", $tmp);
+        } else if (is_numeric($id)) {
+            $sql .= " where `id`='$id'";
+        }
+        $row = $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+        return $row;
+    }
+
+    function del($id)
+    {
+        $sql = "delete from `$this->table` where ";
+        if (is_array($id)) {
+            $tmp = $this->a2s($id);
+            $sql .= join(" && ", $tmp);
+        } else if (is_numeric($id)) {
+            $sql .= " `id`='$id'";
+        }
+        return $this->pdo->exec($sql);
+    }
+
+    
+
+    function all($where = '', $other = '')
+    {
+    }
+    function count($where = '', $other = '')
+    {
+    }
+    function save($sql, $where = '', $other = '')
+    {
+    }
+}
