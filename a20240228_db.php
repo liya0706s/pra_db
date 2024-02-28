f7a5f29f037215f3a25520879d68168bb7fd5bf0
Angie <liya0706s@gmail.com>
2024-02-28 23:47:44 +0800

    feat/fix: class少了建構初始化construct

---
+++ a20240228_db.php
@@ -0,0 +1,29 @@
+<?php
+date_default_timezone_set("Asia/Taipei");
+session_start();
+
+class DB{
+    protected $dsn="mysql:host=localhost;charset=utf8;dbname=db09";
+    protected $pdo;
+    protected $table;
+
+ 
+    function q($sql){}
+    private function a2s($array){}
+    private function sql_all($sql, $where, $other){}
+
+    function all($where='', $other=''){}
+    function count($where='', $other=''){}
+    private function math($col='', $where='', $other=''){}
+    function sum($col='', $where='', $other=''){}
+    function max($col, $where='', $other=''){}
+    function min($col, $where='', $other=''){}
+
+    function find($id){}
+    function del($id){}
+    function save($array){}
+}
+
+
+
+?>
