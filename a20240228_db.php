436ea10d3845e4cabfd7010502ae593f9bd98628
Angie <liya0706s@gmail.com>
2024-02-29 00:01:02 +0800

    feat:打完全部的參數和construct, q, a2s, sql_all..要來對答案囉

--- a20240228_db.php
+++ a20240228_db.php
@@ -7,10 +7,37 @@ class DB{
     protected $pdo;
     protected $table;
 
- 
-    function q($sql){}
-    private function a2s($array){}
-    private function sql_all($sql, $where, $other){}
+    public function __construct($table)
+    {
+        $this->table=$table;
+        $this->pdo=new PDO($this->dsn,'root','');
+    }
+
+    function q($sql){
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    private function a2s($array){
+        foreach($array as $col=>$val){
+            $tmp[]="`$col`='$val'";
+        }
+        return $tmp;
+    }
+
+    private function sql_all($sql, $array, $other){
+        if(isset($this->table) && !empty($this->table)){
+          if(is_array($array)){
+            if(!empty($array)){
+                $tmp=$this->a2s($array);
+            }
+            $sql .= " where ".join(" && ", $tmp);
+          }else{
+            $sql .= " $array";
+          }
+        }
+        $sql.= $other;
+        return $sql;
+    }
 
     function all($where='', $other=''){}
     function count($where='', $other=''){}
