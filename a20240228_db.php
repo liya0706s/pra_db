214c1dfd7a9e63503d4609f4d73e4b71e5bb2b6d
Angie <liya0706s@gmail.com>
2024-02-29 00:02:59 +0800

    fix:sql_all()要小心判斷如果是陣列不為空用a2s方法然後拼接句子接者

--- a20240228_db.php
+++ a20240228_db.php
@@ -2,55 +2,73 @@
 date_default_timezone_set("Asia/Taipei");
 session_start();
 
-class DB{
-    protected $dsn="mysql:host=localhost;charset=utf8;dbname=db09";
+class DB
+{
+    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=db09";
     protected $pdo;
     protected $table;
 
     public function __construct($table)
     {
-        $this->table=$table;
-        $this->pdo=new PDO($this->dsn,'root','');
+        $this->table = $table;
+        $this->pdo = new PDO($this->dsn, 'root', '');
     }
 
-    function q($sql){
+    function q($sql)
+    {
         return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    private function a2s($array){
-        foreach($array as $col=>$val){
-            $tmp[]="`$col`='$val'";
+    private function a2s($array)
+    {
+        foreach ($array as $col => $value) {
+            $tmp[] = "`$col`='$value'";
         }
         return $tmp;
     }
 
-    private function sql_all($sql, $array, $other){
-        if(isset($this->table) && !empty($this->table)){
-          if(is_array($array)){
-            if(!empty($array)){
-                $tmp=$this->a2s($array);
+    private function sql_all($sql, $array, $other)
+    {
+        if (isset($this->table) && !empty($this->table)) {
+            if (is_array($array)) {
+                if (!empty($array)) {
+                    $tmp = $this->a2s($array);
+                    $sql .= " where " . join(" && ", $tmp);
+                }
+            } else {
+                $sql .= " $array";
             }
-            $sql .= " where ".join(" && ", $tmp);
-          }else{
-            $sql .= " $array";
-          }
         }
-        $sql.= $other;
+        $sql .= $other;
         return $sql;
     }
 
-    function all($where='', $other=''){}
-    function count($where='', $other=''){}
-    private function math($col='', $where='', $other=''){}
-    function sum($col='', $where='', $other=''){}
-    function max($col, $where='', $other=''){}
-    function min($col, $where='', $other=''){}
+    function all($where = '', $other = '')
+    {
+    }
+    function count($where = '', $other = '')
+    {
+    }
+    private function math($col = '', $where = '', $other = '')
+    {
+    }
+    function sum($col = '', $where = '', $other = '')
+    {
+    }
+    function max($col, $where = '', $other = '')
+    {
+    }
+    function min($col, $where = '', $other = '')
+    {
+    }
 
-    function find($id){}
-    function del($id){}
-    function save($array){}
+    function find($id)
+    {
+    }
+    function del($id)
+    {
+    }
+    function save($array)
+    {
+    }
 }
-
-
-
-?>
