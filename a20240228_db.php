1101bc0255b95eea95bc0eaa8dbb39c2d5a6e54c
Angie <liya0706s@gmail.com>
2024-02-29 00:55:31 +0800

    feat:finish 計次人數的函式 更新total資料表

--- a20240228_db.php
+++ a20240228_db.php
@@ -130,3 +130,14 @@ function dd($array){
 function to($url){
 header("location:$url");
 }
+
+if(!isset($_SESSION['user'])){
+    if($Total->count(['date'=>date("Y-m-d")])>0){
+        $total=$Total->find(['date'=>date("Y-m-d")]);
+        $total['total']++;
+        $Total->save($total);
+    }else{
+        $Total->save(['date'=>date("Y-m-d"), 'total'=>1]);
+    }
+    $_SESSION['user']=1;
+}
