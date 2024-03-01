fbda3d48cfd854573eec38e3daff166337fc8566
liya0706s <liya0706s@gmail.com>
2024-03-01 11:23:54 +0800

    fix:重要!!!sql_all的參數有三個 sql, array, other, math sum max 和min 參數要放單號

--- b20240301_db.php
+++ b20240301_db.php
@@ -2,34 +2,52 @@
 date_default_timezone_set("Asia/Taipei");
 session_start();
 
-class DB{
-    protected $dsn="mysql:host=localhost;charset=utf8;dbname=db09";
+class DB
+{
+    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=db09";
     protected $pdo;
     protected $table;
 
     public function __construct($table)
     {
-        
     }
 
-    function q($sql){}
-    private function a2s($array){}
-    private function sql_all($sql){}
-    
-    function all($where='',$other=''){}
-    function count($where='', $other=''){}
-    private function math($math, $col, $where, $other){}
-    function sum($col='', $where='', $other=''){}
-    function max($col, $where, $other){}
-    function min($col, $where, $other){}
+    function q($sql)
+    {
+    }
+    private function a2s($array)
+    {
+    }
+    private function sql_all($sql, $array, $other)
+    {
+    }
 
-    function find($id){}
-    function del($id){}
-    function save($array){}
+    function all($where = '', $other = '')
+    {
+    }
+    function count($where = '', $other = '')
+    {
+    }
+    private function math($math, $col, $array = '', $other = '')
+    {
+    }
+    function sum($col = '', $where = '', $other = '')
+    {
+    }
+    function max($col, $where='', $other='')
+    {
+    }
+    function min($col, $where='', $other='')
+    {
+    }
 
+    function find($id)
+    {
+    }
+    function del($id)
+    {
+    }
+    function save($array)
+    {
+    }
 }
-
-
-
-
-?>
