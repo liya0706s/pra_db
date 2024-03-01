ca00214730d01576f01310f6a4ff29e2ec49f277
liya0706s <liya0706s@gmail.com>
2024-03-01 12:35:04 +0800

    0301 pra 2nd time

--- b20240301_db.php
+++ b20240301_db.php
@@ -10,39 +10,80 @@ class DB
 
     public function __construct($table)
     {
+        $this->table = $table;
+        $this->pdo = new PDO($this->dsn, 'root', '');
     }
 
     function q($sql)
     {
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
     private function a2s($array)
     {
+        foreach ($array as $col => $value) {
+            $tmp[] = "`$col`='$value'";
+        }
+        return $tmp;
     }
     private function sql_all($sql, $array, $other)
     {
+        if (isset($this->table) && !empty($this->table)) {
+            if (is_array($array)) {
+                if (!empty($array)) {
+                    $tmp = $this->a2s($array);
+                    $sql .= " where " . join(" && ", $tmp);
+                }
+            } else {
+                $sql .= " $array";
+            }
+            $sql .= $other;
+            return $sql;
+        }
     }
 
     function all($where = '', $other = '')
     {
+        $sql="select * from `$this->table` ";
+        $sql=$this->sql_all($sql, $where, $other);
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
+
     function count($where = '', $other = '')
     {
+        $sql="select count(*) from `$this->table` ";
+        $sql=$this->sql_all($sql, $where, $other);
+        return $this->pdo->query($sql)->fetchColumn();
     }
     private function math($math, $col, $array = '', $other = '')
     {
+        $sql="select $math(`$col`) from `$this->table` ";
+        $sql =$this->sql_all($sql, $array, $other);
+        $this->pdo->query($sql)->fetchColumn();
     }
     function sum($col = '', $where = '', $other = '')
     {
+        return $this->math('sum', $where, $other);
     }
-    function max($col, $where='', $other='')
+    function max($col, $where = '', $other = '')
     {
+        return $this->math('max', $where, $other);
     }
-    function min($col, $where='', $other='')
+    function min($col, $where = '', $other = '')
     {
+        return $this->math('min', $where, $other);
     }
 
     function find($id)
     {
+        $sql="select * from `$this->table` ";
+        if(is_array($id)){
+            $tmp=$this->a2s($id);
+            $sql .= " where ".join(" && ", $tmp);
+        }else if(is_numeric($id)){
+            $sql.= " where `id`='$id'";
+        }
+        $row=$this->pdo->query($sql)
+
     }
     function del($id)
     {
