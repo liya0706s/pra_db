2f04e2cf3a51a02feb0888f03e7a58840e2f745a
Angie <liya0706s@gmail.com>
2024-03-11 15:29:40 +0800

    join()中間的是逗號阿寫成點

--- index.php
+++ index.php
@@ -1,22 +1,9 @@
 <?php
-include_once "./b20240310_db.php";
+include_once "./a20240311_db.php";
 
-// $Title->save(['text'=>'還好安安']);
-// $Title->save(['text'=>'加油', 'id'=>4]);
-// $Title->del(['id'=>3]);
+// CREATE 
+$Title->save(['text'=>'發生什麼事了?','sh'=>1]);
 
-// $title=$Title->all();
-// dd($title);
-
-// $title=$Title->count();
-// dd($title);
-
-// $title=$Title->min('id', ['text'=>'還好安安']);
-$title=$Title->sum('sh');
-dd($title);
-
-// $row=$Title->find(2);
-// dd($row);
 
 
 ?>
