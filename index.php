541b83304aaa2f35137db58421b97ffc9353a2a5
Angie <liya0706s@gmail.com>
2024-03-22 18:37:18 +0800

    2024-03-22 pra

--- index.php
+++ index.php
@@ -1,34 +1,27 @@
 <?php
-include_once "./a20240321_db.php";
+include_once "./a20240322_db.php";
 
 // CRUD 
 
-// CREATE:
-// insert into不行也沒有錯誤訊息?? 是少了return
-// $Title->save(['text'=>'今天去參加北科就業博覽會???', 'sh'=>1]);
-// 沒有寫sh預設值是0
-// $Title->save(['text'=>'今天去參加北科就業博覽會']);
-// $Title->save(['id'=>11, 'text'=>'今天天氣真好!']);
+// CREATE 
+// $Title->save(['text'=>'mp我是誰我是誰我是誰']);
 
-// READ:
+// READ 
 // $all=$Title->all();
 // dd($all);
 
-// UPDATE:
-// $Title->save(['id'=>20,'sh'=>1]);
+// update 
+// $Title->save(['id'=>22, 'text'=>'penny chiu是天使來著']);
 
-// DELETE:
-// $Title->del(['id'=>21]);
+// del 
+// $Title->del(16);
 
 // math 
-// $count=$Title->count(['sh'=>1]);
-// dd($count);
 // $sum=$Title->sum('sh');
 // dd($sum);
 
-// 某日瀏覽總人次
-$date=$Total->find(['date'=>'2024-03-20'])['total'];
-echo $date;
-
-
+// 算單日人數
+$total=$Total->find(['date'=>'2024-03-22'])['total'];
+// dd($total);
+echo $total;
 ?>
---
+++ index_sample2.php
@@ -0,0 +1,34 @@
+<?php
+include_once "./a20240322_db.php";
+
+// CRUD 
+
+// CREATE:
+// insert into不行也沒有錯誤訊息?? 是少了return
+// $Title->save(['text'=>'今天去參加北科就業博覽會???', 'sh'=>1]);
+// 沒有寫sh預設值是0
+// $Title->save(['text'=>'今天去參加北科就業博覽會']);
+// $Title->save(['id'=>11, 'text'=>'今天天氣真好!']);
+
+// READ:
+// $all=$Title->all();
+// dd($all);
+
+// UPDATE:
+// $Title->save(['id'=>20,'sh'=>1]);
+
+// DELETE:
+// $Title->del(['id'=>21]);
+
+// math 
+// $count=$Title->count(['sh'=>1]);
+// dd($count);
+// $sum=$Title->sum('sh');
+// dd($sum);
+
+// 某日瀏覽總人次
+// $date=$Total->find(['date'=>'2024-03-20'])['total'];
+// echo $date;
+
+
+?>
