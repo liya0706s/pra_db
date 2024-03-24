c34ca6449139fbc6ec21d6a550215e76d1b27623
Angie <liya0706s@gmail.com>
2024-03-24 10:20:40 +0800

    fix:有些是.=和單純的=要分清楚, 當日瀏覽人數要用echo 不是dd

--- index.php
+++ index.php
@@ -8,7 +8,23 @@ include_once "./a20240324_db.php";
 // READ all and find 
 // $all=$Title->all();
 // dd($all);
-$find=$Title->find(10);
-dd($find);
+// $find=$Title->find(10);
+// dd($find);
+
+// UPDATE 
+// $Title->save(['id'=>17, 'text'=>'nothing happened']);
+
+// DEL 
+// $Title->del(['id'=>24]);
+
+// math 
+// $max= $Title->max('sh');
+// dd($max);
+// $count=$Title->count('sh');
+// dd($count);
+
+// 算當日人數
+// $total= $Total->find(['date'=>'2024-03-24'])['total'];
+// echo $total;
 
 ?>
